17f539bc6333fa582a7e43738c424c6190150552
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:42:20 +0700

    add method handler getAllStudents

--- student/src/main/java/com/bayu/student/controller/StudentController.java
+++ student/src/main/java/com/bayu/student/controller/StudentController.java
@@ -2,10 +2,14 @@ package com.bayu.student.controller;
 
 import com.bayu.student.dto.CreateStudentRequest;
 import com.bayu.student.dto.CreateStudentResponse;
+import com.bayu.student.dto.StudentDTO;
 import com.bayu.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/students")
@@ -18,6 +22,12 @@ public class StudentController {
     @ResponseStatus(HttpStatus.CREATED)
     public CreateStudentResponse save(@RequestBody CreateStudentRequest request) {
         return studentService.save(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentDTO>> getAllStudents() {
+        List<StudentDTO> allStudents = studentService.getAllStudents();
+        return ResponseEntity.ok(allStudents);
     }
 
 }
