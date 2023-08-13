04a6c967cfc38794c063c5e805b98e0e0821e592
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:26:19 +0700

    inject StudentRepository

--- student/src/main/java/com/bayu/student/service/impl/StudentServiceImpl.java
+++ student/src/main/java/com/bayu/student/service/impl/StudentServiceImpl.java
@@ -1,8 +1,14 @@
 package com.bayu.student.service.impl;
 
+import com.bayu.student.repository.StudentRepository;
 import com.bayu.student.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
 }
