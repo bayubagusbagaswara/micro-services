5c346a4c0d0d5e2ba23e62e0dc780b36b5eb5fe1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:25:47 +0700

    add service impl StudentServiceImpl

---
+++ student/src/main/java/com/bayu/student/service/impl/StudentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bayu.student.service.impl;
+
+import com.bayu.student.service.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+}
