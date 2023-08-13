4544dd56f58cb381e8c8384708304619cd3d0077
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:33:44 +0700

    add dto StudentDTO

---
+++ student/src/main/java/com/bayu/student/dto/StudentDTO.java
@@ -0,0 +1,24 @@
+package com.bayu.student.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDTO {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Integer schoolId;
+
+}
