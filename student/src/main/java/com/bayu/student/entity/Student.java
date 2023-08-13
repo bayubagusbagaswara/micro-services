d7a3fd111a8a27268c9051ab6f86aa5144bcd399
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:23:42 +0700

    add entity Student

---
+++ student/src/main/java/com/bayu/student/entity/Student.java
@@ -0,0 +1,34 @@
+package com.bayu.student.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "school_id")
+    private Integer schoolId;
+}
