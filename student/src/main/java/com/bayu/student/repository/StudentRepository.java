57825d863d7bbc41a90c4439b757ada8edc827ad
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:24:44 +0700

    add repository StudentRepository

---
+++ student/src/main/java/com/bayu/student/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.bayu.student.repository;
+
+import com.bayu.student.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
