15992e7ec552f5d7b2ab32daa82e4b249736e40e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:30:22 +0700

    add method contract save

--- student/src/main/java/com/bayu/student/service/StudentService.java
+++ student/src/main/java/com/bayu/student/service/StudentService.java
@@ -1,4 +1,9 @@
 package com.bayu.student.service;
 
+import com.bayu.student.dto.CreateStudentRequest;
+import com.bayu.student.dto.CreateStudentResponse;
+
 public interface StudentService {
+
+    CreateStudentResponse save(CreateStudentRequest request);
 }
