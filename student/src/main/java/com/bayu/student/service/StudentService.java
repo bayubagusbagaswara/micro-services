bf05525df8090bef6cf386782e40736988f4111b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:34:21 +0700

    add method contract getAllStudents

--- student/src/main/java/com/bayu/student/service/StudentService.java
+++ student/src/main/java/com/bayu/student/service/StudentService.java
@@ -2,8 +2,14 @@ package com.bayu.student.service;
 
 import com.bayu.student.dto.CreateStudentRequest;
 import com.bayu.student.dto.CreateStudentResponse;
+import com.bayu.student.dto.StudentDTO;
+
+import java.util.List;
 
 public interface StudentService {
 
     CreateStudentResponse save(CreateStudentRequest request);
+
+    List<StudentDTO> getAllStudents();
+
 }
