213cb1266d53bc609e32cede7c2197a32c77a110
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:32:44 +0700

    implement method save

--- student/src/main/java/com/bayu/student/service/impl/StudentServiceImpl.java
+++ student/src/main/java/com/bayu/student/service/impl/StudentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.bayu.student.service.impl;
 
+import com.bayu.student.dto.CreateStudentRequest;
+import com.bayu.student.dto.CreateStudentResponse;
+import com.bayu.student.entity.Student;
 import com.bayu.student.repository.StudentRepository;
 import com.bayu.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
@@ -10,5 +13,24 @@ import org.springframework.stereotype.Service;
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
+
+    @Override
+    public CreateStudentResponse save(CreateStudentRequest request) {
+        Student student = new Student();
+        student.setFirstName(request.getFirstName());
+        student.setLastName(request.getLastName());
+        student.setEmail(request.getEmail());
+        student.setSchoolId(request.getSchoolId());
+
+        studentRepository.save(student);
+
+        return CreateStudentResponse.builder()
+                .id(student.getId())
+                .firstName(student.getFirstName())
+                .lastName(student.getLastName())
+                .email(student.getEmail())
+                .schoolId(student.getSchoolId())
+                .build();
+    }
 
 }
