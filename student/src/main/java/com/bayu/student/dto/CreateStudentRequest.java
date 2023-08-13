52f79db6dd64952a8d094c6bd26b4c3357da544c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-13 11:29:37 +0700

    add dto CreateStudentRequest

---
+++ student/src/main/java/com/bayu/student/dto/CreateStudentRequest.java
@@ -0,0 +1,19 @@
+package com.bayu.student.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateStudentRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
