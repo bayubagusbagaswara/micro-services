a2fe8a92d087e1fa841aaaf5edea8bfc4de36180
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:03:46 +0700

    add method save

--- school/src/main/java/com/bayu/school/service/impl/SchoolServiceImpl.java
+++ school/src/main/java/com/bayu/school/service/impl/SchoolServiceImpl.java
@@ -1,8 +1,16 @@
 package com.bayu.school.service.impl;
 
+import com.bayu.school.dto.CreateSchoolRequest;
+import com.bayu.school.dto.CreateSchoolResponse;
 import com.bayu.school.service.SchoolService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SchoolServiceImpl implements SchoolService {
+
+    @Override
+    public CreateSchoolResponse save(CreateSchoolRequest request) {
+        return null;
+    }
+
 }
