59c2e74ac8f08f3410c69119c47a93fa7cedc845
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:03:21 +0700

    add method contract getAllSchools

--- school/src/main/java/com/bayu/school/service/SchoolService.java
+++ school/src/main/java/com/bayu/school/service/SchoolService.java
@@ -2,8 +2,13 @@ package com.bayu.school.service;
 
 import com.bayu.school.dto.CreateSchoolRequest;
 import com.bayu.school.dto.CreateSchoolResponse;
+import com.bayu.school.dto.SchoolDTO;
+
+import java.util.List;
 
 public interface SchoolService {
 
     CreateSchoolResponse save(CreateSchoolRequest request);
+
+    List<SchoolDTO> getAllSchools();
 }
