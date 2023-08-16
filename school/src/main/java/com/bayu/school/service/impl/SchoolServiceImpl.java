9081ca449c25a011386311bc2d6588b277b3f93d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:02:16 +0700

    add service impl SchoolServiceImpl

---
+++ school/src/main/java/com/bayu/school/service/impl/SchoolServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bayu.school.service.impl;
+
+import com.bayu.school.service.SchoolService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchoolServiceImpl implements SchoolService {
+}
