b44022c1c60fdd5e8aee424cb7516f636b12796f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:02:52 +0700

    add method contract save

--- school/src/main/java/com/bayu/school/service/SchoolService.java
+++ school/src/main/java/com/bayu/school/service/SchoolService.java
@@ -1,4 +1,9 @@
 package com.bayu.school.service;
 
+import com.bayu.school.dto.CreateSchoolRequest;
+import com.bayu.school.dto.CreateSchoolResponse;
+
 public interface SchoolService {
+
+    CreateSchoolResponse save(CreateSchoolRequest request);
 }
