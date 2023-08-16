c7b72bed95d3e8e30f25905edbb33d8d87f2720d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:04:30 +0700

    inject SchoolRepository

--- school/src/main/java/com/bayu/school/service/impl/SchoolServiceImpl.java
+++ school/src/main/java/com/bayu/school/service/impl/SchoolServiceImpl.java
@@ -3,13 +3,18 @@ package com.bayu.school.service.impl;
 import com.bayu.school.dto.CreateSchoolRequest;
 import com.bayu.school.dto.CreateSchoolResponse;
 import com.bayu.school.dto.SchoolDTO;
+import com.bayu.school.repository.SchoolRepository;
 import com.bayu.school.service.SchoolService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SchoolServiceImpl implements SchoolService {
+
+    private final SchoolRepository schoolRepository;
 
     @Override
     public CreateSchoolResponse save(CreateSchoolRequest request) {
