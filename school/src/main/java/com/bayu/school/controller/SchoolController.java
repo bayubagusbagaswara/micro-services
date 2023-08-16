df490931c7352c9795520af2c56f5f85b2f6e4f5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:14:14 +0700

    add method getAllSchools

--- school/src/main/java/com/bayu/school/controller/SchoolController.java
+++ school/src/main/java/com/bayu/school/controller/SchoolController.java
@@ -2,10 +2,14 @@ package com.bayu.school.controller;
 
 import com.bayu.school.dto.CreateSchoolRequest;
 import com.bayu.school.dto.CreateSchoolResponse;
+import com.bayu.school.dto.SchoolDTO;
 import com.bayu.school.service.SchoolService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/schools")
@@ -18,6 +22,12 @@ public class SchoolController {
     @ResponseStatus(HttpStatus.CREATED)
     public CreateSchoolResponse save(@RequestBody CreateSchoolRequest request) {
         return schoolService.save(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SchoolDTO>> getAllSchools() {
+        List<SchoolDTO> allSchools = schoolService.getAllSchools();
+        return ResponseEntity.ok(allSchools);
     }
 
 }
