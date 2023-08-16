31378d38067c791eef1a69b7b6bfa8d712e46d7d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 08:58:14 +0700

    add repository SchoolRepository

---
+++ school/src/main/java/com/bayu/school/repository/SchoolRepository.java
@@ -0,0 +1,10 @@
+package com.bayu.school.repository;
+
+import com.bayu.school.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Integer> {
+
+}
