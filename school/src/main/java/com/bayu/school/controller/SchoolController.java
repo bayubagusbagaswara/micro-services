1339f91aee7f4099714c97e5ae93083dafa328f5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-16 09:10:09 +0700

    add controller SchoolController

---
+++ school/src/main/java/com/bayu/school/controller/SchoolController.java
@@ -0,0 +1,11 @@
+package com.bayu.school.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/schools")
+@RequiredArgsConstructor
+public class SchoolController {
+}
