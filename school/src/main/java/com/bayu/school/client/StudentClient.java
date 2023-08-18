cfd20fbe1d1af120a5e5ea014ab9a4c4b2b5efea
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-18 09:36:21 +0700

    add method findAllStudents

--- school/src/main/java/com/bayu/school/client/StudentClient.java
+++ school/src/main/java/com/bayu/school/client/StudentClient.java
@@ -1,7 +1,16 @@
 package com.bayu.school.client;
 
+import com.bayu.school.dto.StudentDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "student-service", url = "${application.config.students-url}")
 public interface StudentClient {
+
+    @GetMapping(path = "/school/{school-id}")
+    List<StudentDTO> findAllStudentsBySchool(@PathVariable(name = "school-id") Integer schoolId);
+
 }
